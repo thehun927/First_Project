22a4095845496d2fd28c3610dbb5da5869ffcd22
thehun927 <thehun927@gmail.com>
2014-03-20 06:47:07 -0400

    choose your own directory

--- Virus Scanner Linux.py
+++ Virus Scanner Linux.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 
+import csv
+import sys
 import os
 import hashlib
 
-path = "/home/attila/"
+#path = "/home/attila/GitHub"
+path = raw_input("Enter a directory: ")
 workingdir = os.getcwd()
 print "Current working directory %s" % workingdir
 
@@ -13,14 +16,9 @@ workingdir = os.getcwd()
 
 print "Directory changed successfully to %s" % workingdir
 
-for root, dirs, files in os.walk(".", followlinks=False):
+for root, dirs, files in os.walk(onerror=None, topdown=True, followlinks=False):
     for name in files:
         print(os.path.join(root, name))
     for name in dirs:
         print(os.path.join(root, name))
-
-
-
-
-
 
