25a997e28f12f92c10eda0c60dc495d569cef7d7
Attila Gyurovszky <thehun927@gmail.com>
2014-03-22 17:52:45 -0400

    Search and cross reference MD5 made list and one downloaded

--- Virus Scanner Mac.py
+++ Virus Scanner Mac.py
@@ -7,33 +7,33 @@ import time
 import hashlib
 import difflib
 from progressbar \
-    import Bar,  ETA, ProgressBar, ReverseBar
+	import Bar,  ETA, ProgressBar, ReverseBar
 
 def md5Checksum(filePath):                      #Function to generate MD5 Hash
-    with open(filePath, 'rb') as fh:
-        m = hashlib.md5()
-        while True:
-            data = fh.read(8192)
-            if not data:
-                break
-            m.update(data)
-        return m.hexdigest()        #Function to generate MD5 Hash
+	with open(filePath, 'rb') as fh:
+		m = hashlib.md5()
+		while True:
+			data = fh.read(8192)
+			if not data:
+				break
+			m.update(data)
+		return m.hexdigest()        #Function to generate MD5 Hash
 
 def countdown():
-    print "Starting in 3.."
-    time.sleep(1)
-    print "2.."
-    time.sleep(1)
-    print "1.."
-    time.sleep(1)
+	print "Starting in 3.."
+	time.sleep(1)
+	print "2.."
+	time.sleep(1)
+	print "1.."
+	time.sleep(1)
 
 def backwardsBar():
-    widgets = [Bar('>'), ' ', ETA(), ' ', ReverseBar('<')]
-    pbar = ProgressBar(widgets=widgets, maxval=10000000).start()
-    for i in range(1000000):
-        # do something
-        pbar.update(10*i+1)
-    pbar.finish()
+	widgets = [Bar('>'), ' ', ETA(), ' ', ReverseBar('<')]
+	pbar = ProgressBar(widgets=widgets, maxval=10000000).start()
+	for i in range(1000000):
+		# do something
+		pbar.update(10*i+1)
+	pbar.finish()
 
 md5 = hashlib.md5()
 Vlog = "/Users/attila/Desktop/Vlog.txt"
@@ -44,24 +44,28 @@ path = raw_input("Enter a directory to scan: ")         #Custom directory scanni
 os.chdir(path)                            #Change the cwd to the user specified path
 workingdir = os.getcwd()                   #set workingdir to the cwd
 
-print "Directory changed successfully to %s" % workingdir       #Prints the changed directory for reference
-
-#countdown()     #Countdown function
-#backwardsBar()
+print "Directory changed successfully to %s" % workingdir + "\n"      #Prints the changed directory for reference
 
 seq = []
 for root, dirs, files in os.walk(path,onerror=None, topdown=True, followlinks=False):   #Scan directories and files in the path variable
-    for name in files:                          #For every file name in files scanned
-        seq.append(str(os.path.join(root, name)))          #Write the path and name of the file
-        #seq.append(str('\n'))
+	for name in files:                          #For every file name in files scanned
+		seq.append(str(os.path.join(root, name)))          #Write the path and name of the file
+		#seq.append(str('\n'))
 
 md = []
 for item in seq:
-    md5Checksum(item)
-    md.append(md5Checksum(item))
-    #md.append('\n')
+	md5Checksum(item)
+	md.append(md5Checksum(item))
+	#md.append('\n')
 
 Vlist = [line.strip() for line in open(Vlog, 'r')]
 
-for index, item in enumerate(Vlist):
-    print index, item
+for item in md:
+    if item in Vlist:
+        print True
+        print item
+
+
+
+
+
