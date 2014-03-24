0c4b97aae21c0b95f4315395cd189c64362c6289
thehun927 <thehun927@gmail.com>
2014-03-24 13:43:43 -0400

    updated Linux version

--- Virus Scanner Linux.py
+++ Virus Scanner Linux.py
@@ -5,8 +5,12 @@ import sys
 import os
 import time
 import hashlib
+import difflib
+from progressbar \
+    import Bar, ETA, ProgressBar, ReverseBar
 
-def md5Checksum(filePath):
+
+def md5Checksum(filePath):  #Function to generate MD5 Hash
     with open(filePath, 'rb') as fh:
         m = hashlib.md5()
         while True:
@@ -14,37 +18,59 @@ def md5Checksum(filePath):
             if not data:
                 break
             m.update(data)
-        return m.hexdigest()
+        return m.hexdigest()  #Function to generate MD5 Hash
 
-#path = "/home/attila/GitHub"
+
+def countdown():
+    print "Starting in 3.."
+    time.sleep(1)
+    print "2.."
+    time.sleep(1)
+    print "1.."
+    time.sleep(1)
+
+
+def backwardsBar():
+    widgets = [Bar('>'), ' ', ETA(), ' ', ReverseBar('<')]
+    pbar = ProgressBar(widgets=widgets, maxval=10000000).start()
+    for i in range(1000000):
+        # do something
+        pbar.update(10 * i + 1)
+    pbar.finish()
+
+
 md5 = hashlib.md5()
+Vlog = "/home/attila/Desktop/Vlog.txt"
 
-log = "/home/attila/Desktop/log.txt"
+print "Current working directory %s" % os.getcwd()  #Prints the current working directory for reference
+path = raw_input("Enter a directory to scan: ")  #Custom directory scanning
 
-print "Current working directory %s" % os.getcwd()
-path = raw_input("Enter a directory to scan: ")
+os.chdir(path)  #Change the cwd to the user specified path
+workingdir = os.getcwd()  #set workingdir to the cwd
 
-os.chdir(path)
-workingdir = os.getcwd()
+print "Directory changed successfully to %s" % workingdir + "\n"  #Prints the changed directory for reference
 
-print "Directory changed successfully to %s" % workingdir
+seq = []
+for root, dirs, files in os.walk(path, onerror=None, topdown=True,
+                                 followlinks=False):  #Scan directories and files in the path variable
+    for name in files:  #For every file name in files scanned
+        seq.append(str(os.path.join(root, name)))  #Write the path and name of the file
+    #seq.append(str('\n'))
 
-print "Starting in 3.."
-time.sleep(1)
-print "2.."
-time.sleep(1)
-print "1.."
-time.sleep(1)
+md = []
+for item in seq:
+    md5Checksum(item)
+    md.append(md5Checksum(item))
+#md.append('\n')
 
-with open(log, 'w') as F:
-    for root, dirs, files in os.walk(path,onerror=None, topdown=True, followlinks=False):
-        for name in files:
-#            print(os.path.join(root, name))
-            F.write(str(os.path.join(root, name)))
-            F.write(str('\n'))
+Vlist = [line.strip() for line in open(Vlog, 'r')]
+
+for item in md:
+    if item in Vlist:
+        print True
+        print item
 
 
-F = open(log).read().splitlines()
-for line in F:
-    md5Checksum(line)
-    print("The MD5 Checksum of the file is ", md5Checksum(line))
+
+
+
